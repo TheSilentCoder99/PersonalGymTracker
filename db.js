cfc8ae2f5b4f8d59ae02ff3edc4f25dbb8b36fa9
Alan <alrd1718@gmail.com>
2026-07-17 23:51:21 +0200

    aumentada versión de la db y del manifest

--- db.js
+++ db.js
@@ -3,7 +3,7 @@ let conexion;
 function inicializarBaseDeDatos() {
     return new Promise((resolve, reject) => {
 
-        const request = indexedDB.open("gymtracker", 6);
+        const request = indexedDB.open("gymtracker", 9);
 
         request.onupgradeneeded = (e) => {
             const db = e.target.result;
